694d2872f1915768394171471afc9388721461ae
Di M <meerkdir@gmail.com>
2024-05-30 11:37:06 +0200

    go

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+
+import BillTracker from './components/BillTracker';
+
 import './app.scss';
+
+
 
 function App() {
   const [state, setState] = React.useState("Hello, world!");
   const [counter, setCounter] = React.useState(0); 
 
   return (
-    <div className="mx-auto max-w-md px-4 py-8">
-      <p>{state}</p>
-      <button onClick={() => setState("Hello, React!")}>Change Text</button>
-      <p>Counter: {counter}</p>
-      <button onClick={() => setCounter(counter + 1)}>Increment</button> 
-    </div>
+    <BillTracker />
   );
 }
 
