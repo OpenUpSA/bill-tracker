c6bc23fb4c289edfd4f28c54db82dca8b2daf3e5
paulmwatson <github.com@paulmwatson.com>
2024-07-03 17:51:50 +0200

    Tweaks to header and parliament selector

--- src/components/attendance/index.js
+++ src/components/attendance/index.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
+import Stack from "react-bootstrap/Stack";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCheck } from "@fortawesome/free-solid-svg-icons";
@@ -26,46 +27,38 @@ function Attendance() {
     <div className="attendance-tracker">
       <header className="p-5">
         <Container fluid>
-          <Row>
-            <Col md={8}>
-              <h1>
-                <FontAwesomeIcon icon={faUserCheck} /> Attendance
-              </h1>
-              <span>
-                Which parties and members are attending the most meetings.
-              </span>
-            </Col>
-            <Col>
-              <Row>
-                <Col>
-                  <Form.Group as={Row}>
-                    <Form.Label column md="auto">
-                      Parliament
-                    </Form.Label>
-                    <Col>
-                      <Form.Select
-                        aria-label="Select Parliament"
-                        defaultValue={selectedParliament}
-                        onChange={(e) => changeParliament(e.target.value)}
-                        size="lg"
-                      >
-                        {Object.keys(parliaments).map((parliament, index) => {
-                          return (
-                            <option
-                              value={parliament}
-                              key={`parliament-${index}`}
-                            >
-                              {lookup.parliaments[parliament].name}
-                            </option>
-                          );
-                        })}
-                      </Form.Select>
-                    </Col>
-                  </Form.Group>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
+          <Stack direction="horizontal" gap="3">
+            <h1>
+              <Stack direction="horizontal" gap="3">
+                <FontAwesomeIcon icon={faUserCheck} /> <div>Attendance</div>
+              </Stack>
+            </h1>
+            <div style={{ marginLeft: "auto" }}>Parliament:</div>
+            <Form.Select
+              aria-label="Select Parliament"
+              defaultValue={selectedParliament}
+              onChange={(e) => changeParliament(e.target.value)}
+              size="md"
+              className="parliamentSelector"
+            >
+              {Object.keys(parliaments).map((parliament, index) => {
+                return (
+                  <option value={parliament} key={`parliament-${index}`}>
+                    {lookup.parliaments[parliament].name} (
+                    {new Date(
+                      lookup.parliaments[parliament].start
+                    ).getFullYear()}
+                    -
+                    {new Date(lookup.parliaments[parliament].end).getFullYear()}
+                    )
+                  </option>
+                );
+              })}
+            </Form.Select>
+          </Stack>
+          <span>
+            Which parties and members are attending the most meetings.
+          </span>
         </Container>
       </header>
       <Container fluid>
