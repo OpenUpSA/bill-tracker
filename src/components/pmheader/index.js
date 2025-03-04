6d42c7d306c7be5fc032f89ce0bd760ead17fe35
desafinadude <meerkdir@gmail.com>
2025-03-04 11:37:44 +0200

    updated header links

--- src/components/pmheader/index.js
+++ src/components/pmheader/index.js
@@ -27,16 +27,16 @@ function PMHeader() {
 					<Col>
 						<Nav className="justify-content-end">
 							<Nav.Item>
-								<Nav.Link href="/">ParliMeter Home</Nav.Link>
+								<Nav.Link href="https://parlimeter.co.za">ParliMeter Home</Nav.Link>
 							</Nav.Item>
 							<Nav.Item>
-								<Nav.Link href="/about">About</Nav.Link>
+								<Nav.Link href="https://parlimeter.co.za/about">About</Nav.Link>
 							</Nav.Item>
 							<Nav.Item>
-								<Nav.Link href="/blog">Blog</Nav.Link>
+								<Nav.Link href="https://parlimeter.co.za/blog">Blog</Nav.Link>
 							</Nav.Item>
 							<Nav.Item>
-								<Nav.Link href="/data-stories">Data Stories</Nav.Link>
+								<Nav.Link href="https://parlimeter.co.za/recommendations">Recommendations</Nav.Link>
 							</Nav.Item>
 						</Nav>
 					</Col>
